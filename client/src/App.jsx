4e1cc7db5291bda409be6d433711c268b0b2cc8a
nurfadhilaheryanti <nurfadhilaheryanti@gmail.com>
2024-06-19 11:45:42 +0700

    implemented socket.io, peerjs not done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef, useState } from "react";
+import Peer from "simple-peer";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io.connect("http://localhost:5001");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+      setStream(stream);
+      myVideo.current.srcObject = stream;
+    });
+
+    socket.on("me", (id) => {
+      setMe(id);
+    });
+
+    socket.on("callUser", (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+  }, []);
+
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name,
+      });
+    });
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller });
+    });
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="video-container">
+          <div className="video">
+            {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+          </div>
+          <div className="video">
+            {callAccepted && !callEnded ? (
+              <video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} />
+            ) : null}
+          </div>
+        </div>
+        <div className="myId">
+          <input
+            id="filled-basic"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginBottom: "20px", padding: "10px" }}
+          />
+          <button
+            onClick={() => navigator.clipboard.writeText(me)}
+            style={{ marginBottom: "2rem", padding: "10px", backgroundColor: "blue", color: "white" }}
+          >
+            Copy ID
+          </button>
+
+          <input
+            id="filled-basic"
+            type="text"
+            placeholder="ID to call"
+            value={idToCall}
+            onChange={(e) => setIdToCall(e.target.value)}
+            style={{ padding: "10px", marginBottom: "20px" }}
+          />
+          <div className="call-button">
+            {callAccepted && !callEnded ? (
+              <button onClick={leaveCall} style={{ padding: "10px", backgroundColor: "red", color: "white" }}>
+                End Call
+              </button>
+            ) : (
+              <button onClick={() => callUser(idToCall)} style={{ padding: "10px", backgroundColor: "green", color: "white" }}>
+                Call
+              </button>
+            )}
+            <span>{idToCall}</span>
+          </div>
+        </div>
+        <div>
+          {receivingCall && !callAccepted ? (
+            <div className="caller">
+              <h1>{name} is calling...</h1>
+              <button onClick={answerCall} style={{ padding: "10px", backgroundColor: "blue", color: "white" }}>
+                Answer
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
