1264743846d6d1fd65a4a174607422b3f54c0846
jafarbdurr <jafarbdurr@gmail.com>
2024-06-20 10:34:37 +0700

    speech recognition

---
+++ client/src/hooks/useSpeechToText/index.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react"
+
+const UseSpeechToText = (options) => {
+    const [isListening, setIsListening] = useState(false)
+    const [transcript, setTranscript] = useState("")
+    const recgonitionRef = useRef (null)
+
+    useEffect( () => {
+        if(!('webkitSpeechRecognition' in window)) {
+            console.error('Web speech api is not supported');
+            return
+        }
+        recgonitionRef.current = new window.webkitSpeechRecognition()
+        const recognition = recgonitionRef.current
+        recognition.interimResults = options.interimResults || true
+        recognition.lang = options.lang || 'en-US'
+        recognition.continous = options.continous || false
+
+        if("webkitSpeechGrammarList" in window){
+            const grammar = '#JSGF V1.0; grammar punctuation; public <punc> = . | , | ? | ! | ; | : ;'
+            const speechRecognitionList = new window.webkitSpeechGrammarList()
+            speechRecognitionList.addFromString(grammar, 1)
+            recognition.grammars = speechRecognitionList
+        }
+
+        recognition.onresult = (event) => {
+            let text = ''
+            for (let i = 0; i < event.results.length; i++) {
+                text += event.results[i][0].transcript
+            }
+            setTranscript(text)
+        }
+
+        recognition.onerror = (event) => {
+            console.error("speech recognition error:", event.error)
+        }
+
+        recognition.onend = () => {
+            setIsListening(false)
+            setTranscript("")
+        }
+
+        return () => {
+            recognition.stop
+        }
+
+    }, [])
+
+    const startListening = () => {
+        if(recgonitionRef.current && !isListening){
+            recgonitionRef.current.start()
+            setIsListening(true)
+        }
+    }
+    const stopListening = () => {
+        if(recgonitionRef.current && isListening){
+            recgonitionRef.current.stop()
+            setIsListening(false)
+        }
+    }
+
+    return {
+        isListening,
+        transcript,
+        startListening,
+        stopListening
+    }
+}
+
+export default UseSpeechToText
